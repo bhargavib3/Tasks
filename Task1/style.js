e1529514097dca01caffd4900d757474c8cf55d3
bhargavib3 <138289798+bhargavib3@users.noreply.github.com>
2023-07-01 19:58:03 +0530

    Add files via upload

---
+++ Task1/style.js
@@ -0,0 +1,12 @@
+function openNavbar() {
+document.getElementById("sideNavigationBar")
+.style.width = "50%";
+
+}
+
+function closeNavbar() {
+document.getElementById("sideNavigationBar")
+
+.style.width="0%";
+
+}
